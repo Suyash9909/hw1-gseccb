2921440a4e7796d0c805ce0820cd7ad66fb0bcb2
Elliott Braverman <ebraverm@stevens.edu>
2023-03-05 10:43:21 -0500

    2nd attempt

--- answer.py
+++ answer.py
@@ -126,7 +126,7 @@ def dictionary_nested():
 
     # Get the value of key "last_name" from the subdictionary of key "name" in dictionary Grace. (aka."Hopper")
 
-    last_name = Grace["name"["last_name"]]
+    last_name = Grace["name"[1]]
 
     # Add "programmer" to the list that key "jobs" maps to.
 
